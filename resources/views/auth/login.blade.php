afd38ebeeecf1b6e91b91f38307df93ab3bdb48a
infomotin <motin.mmk.jr@gmail.com>
2024-03-17 21:56:14 +0600

    Frontend Theme splite with Login Page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,4 @@
-<x-guest-layout>
+{{-- <x-guest-layout>
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
@@ -44,4 +44,136 @@
             </x-primary-button>
         </div>
     </form>
-</x-guest-layout>
+</x-guest-layout> --}}
+
+@extends('forntend.frontend_dashboard')
+@section('main_content')
+    <!--Page Title-->
+    <section class="page-title-two bg-color-1 centred">
+        <div class="pattern-layer">
+            <div class="pattern-1" style="background-image: url(assets/images/shape/shape-9.png);"></div>
+            <div class="pattern-2" style="background-image: url(assets/images/shape/shape-10.png);"></div>
+        </div>
+        <div class="auto-container">
+            <div class="content-box clearfix">
+                <h1>Sign Up</h1>
+                <ul class="bread-crumb clearfix">
+                    <li><a href="index.html">Home</a></li>
+                    <li>Sign Up</li>
+                </ul>
+            </div>
+        </div>
+    </section>
+    <!--End Page Title-->
+
+
+    <!-- ragister-section -->
+    <section class="ragister-section centred sec-pad">
+        <div class="auto-container">
+            <div class="row clearfix">
+                <div class="col-xl-8 col-lg-12 col-md-12 offset-xl-2 big-column">
+                    <div class="sec-title">
+                        <h5>Sign up</h5>
+                        <h2>Sign In With Realshed</h2>
+                    </div>
+                    <div class="tabs-box">
+                        <div class="tab-btn-box">
+                            <ul class="tab-btns tab-buttons centred clearfix">
+                                <li class="tab-btn active-btn" data-tab="#tab-1">Agent</li>
+                                <li class="tab-btn" data-tab="#tab-2">User</li>
+                            </ul>
+                        </div>
+                        <div class="tabs-content">
+                            <div class="tab active-tab" id="tab-1">
+                                <div class="inner-box">
+                                    <h4>Sign up</h4>
+                                    <form action="signin.html" method="post" class="default-form">
+                                        <div class="form-group">
+                                            <label>Agent name</label>
+                                            <input type="text" name="name" required="">
+                                        </div>
+                                        <div class="form-group">
+                                            <label>Email address</label>
+                                            <input type="email" name="email" required="">
+                                        </div>
+                                        <div class="form-group">
+                                            <label>New Password</label>
+                                            <input type="password" name="name" required="">
+                                        </div>
+                                        <div class="form-group">
+                                            <label>Confirm Password</label>
+                                            <input type="password" name="name" required="">
+                                        </div>
+                                        <div class="form-group message-btn">
+                                            <button type="submit" class="theme-btn btn-one">Sign up</button>
+                                        </div>
+                                    </form>
+                                    <div class="othre-text">
+                                        <p>Already have an account? <a href="signin.html">Sign in</a></p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="tab" id="tab-2">
+                                <div class="inner-box">
+                                    <h4>Sign up</h4>
+                                    <form action="signin.html" method="post" class="default-form">
+                                        <div class="form-group">
+                                            <label>User name</label>
+                                            <input type="text" name="name" required="">
+                                        </div>
+                                        <div class="form-group">
+                                            <label>Email address</label>
+                                            <input type="email" name="email" required="">
+                                        </div>
+                                        <div class="form-group">
+                                            <label>New Password</label>
+                                            <input type="password" name="name" required="">
+                                        </div>
+                                        <div class="form-group">
+                                            <label>Confirm Password</label>
+                                            <input type="password" name="name" required="">
+                                        </div>
+                                        <div class="form-group message-btn">
+                                            <button type="submit" class="theme-btn btn-one">Sign up</button>
+                                        </div>
+                                    </form>
+                                    <div class="othre-text">
+                                        <p>Already have an account? <a href="signin.html">Sign in</a></p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- ragister-section end -->
+
+
+    <!-- subscribe-section -->
+    <section class="subscribe-section bg-color-3">
+        <div class="pattern-layer" style="background-image: url(assets/images/shape/shape-2.png);"></div>
+        <div class="auto-container">
+            <div class="row clearfix">
+                <div class="col-lg-6 col-md-6 col-sm-12 text-column">
+                    <div class="text">
+                        <span>Subscribe</span>
+                        <h2>Sign Up To Our Newsletter To Get The Latest News And Offers.</h2>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-6 col-sm-12 form-column">
+                    <div class="form-inner">
+                        <form action="contact.html" method="post" class="subscribe-form">
+                            <div class="form-group">
+                                <input type="email" name="email" placeholder="Enter your email" required="">
+                                <button type="submit">Subscribe Now</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- subscribe-section end -->
+@endsection
