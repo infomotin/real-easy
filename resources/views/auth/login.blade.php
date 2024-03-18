b89ad03024a7eb83b0e5db9d65616b552e4e5dd2
infomotin <motin.mmk.jr@gmail.com>
2024-03-18 20:11:00 +0600

    custom log/reg

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -51,8 +51,8 @@
     <!--Page Title-->
     <section class="page-title-two bg-color-1 centred">
         <div class="pattern-layer">
-            <div class="pattern-1" style="background-image: url(assets/images/shape/shape-9.png);"></div>
-            <div class="pattern-2" style="background-image: url(assets/images/shape/shape-10.png);"></div>
+            <div class="pattern-1" style="background-image: url({{ asset('Frontend/assets/images/shape/shape-9.png')}};"></div>
+            <div class="pattern-2" style="background-image: url({{ asset('Forntend/assets/images/shape/shape-10.png')}});"></div>
         </div>
         <div class="auto-container">
             <div class="content-box clearfix">
@@ -79,66 +79,91 @@
                     <div class="tabs-box">
                         <div class="tab-btn-box">
                             <ul class="tab-btns tab-buttons centred clearfix">
-                                <li class="tab-btn active-btn" data-tab="#tab-1">Agent</li>
-                                <li class="tab-btn" data-tab="#tab-2">User</li>
+                                <li class="tab-btn active-btn" data-tab="#tab-1">Login</li>
+                                <li class="tab-btn" data-tab="#tab-2">Register</li>
                             </ul>
                         </div>
                         <div class="tabs-content">
                             <div class="tab active-tab" id="tab-1">
                                 <div class="inner-box">
-                                    <h4>Sign up</h4>
-                                    <form action="signin.html" method="post" class="default-form">
-                                        <div class="form-group">
-                                            <label>Agent name</label>
-                                            <input type="text" name="name" required="">
+                                    <h4>Login</h4>
+                                    <form method="POST" action="{{ route('login') }}" class="default-form">
+                                        @csrf
+                                        <div class="form-group @error('login') has-error @enderror">
+                                            <label>Username/Phone/Email</label>
+                                            <input type="text" name="login" id="login" required="">
+                                            @error('login')
+                                                <span class="help-block">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                            @enderror
                                         </div>
-                                        <div class="form-group">
-                                            <label>Email address</label>
-                                            <input type="email" name="email" required="">
+                                        <div class="form-group @error('password') has-error @enderror">
+                                            <label>Password</label>
+                                            <input type="password" name="password" name="password" required="">
+                                            @error('password')
+                                                <span class="help-block">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                            @enderror
                                         </div>
-                                        <div class="form-group">
-                                            <label>New Password</label>
-                                            <input type="password" name="name" required="">
-                                        </div>
-                                        <div class="form-group">
-                                            <label>Confirm Password</label>
-                                            <input type="password" name="name" required="">
-                                        </div>
+                                        
                                         <div class="form-group message-btn">
-                                            <button type="submit" class="theme-btn btn-one">Sign up</button>
+                                            <button type="submit" class="theme-btn btn-one">Login</button>
                                         </div>
                                     </form>
                                     <div class="othre-text">
-                                        <p>Already have an account? <a href="signin.html">Sign in</a></p>
+                                        <p>No Account? <a href="{{ route('register') }}">REgister</a></p>
                                     </div>
                                 </div>
                             </div>
                             <div class="tab" id="tab-2">
                                 <div class="inner-box">
-                                    <h4>Sign up</h4>
-                                    <form action="signin.html" method="post" class="default-form">
-                                        <div class="form-group">
+                                    <h4>Register</h4>
+                                    <form  method="POST" action="{{ route('register') }}" class="default-form">
+                                        @csrf
+                                        <div class="form-group @error('name') has-error @enderror">
                                             <label>User name</label>
-                                            <input type="text" name="name" required="">
+                                            <input type="text" name="name" id="name" required="">
+                                            @error('name')
+                                                <span class="help-block">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                            @enderror
                                         </div>
-                                        <div class="form-group">
+                                        <div class="form-group @error('email') has-error @enderror">
                                             <label>Email address</label>
                                             <input type="email" name="email" required="">
+                                            @error('email')
+                                                <span class="help-block">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                            @enderror
                                         </div>
-                                        <div class="form-group">
+                                        <div class="form-group @error('password') has-error @enderror">
                                             <label>New Password</label>
-                                            <input type="password" name="name" required="">
+                                            <input type="password" name="password" id="password" required="">
+                                            @error('password')
+                                                <span class="help-block">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                            @enderror
                                         </div>
-                                        <div class="form-group">
+                                        <div class="form-group @error('password_confirmation') has-error @enderror">
                                             <label>Confirm Password</label>
-                                            <input type="password" name="name" required="">
+                                            <input type="password" name="password_confirmation" id="password_confirmation" required="">
+                                            @error('password_confirmation')
+                                                <span class="help-block">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                            @enderror
                                         </div>
                                         <div class="form-group message-btn">
-                                            <button type="submit" class="theme-btn btn-one">Sign up</button>
+                                            <button type="submit" class="theme-btn btn-one">Register </button>
                                         </div>
                                     </form>
                                     <div class="othre-text">
-                                        <p>Already have an account? <a href="signin.html">Sign in</a></p>
+                                        <p>Already have an account? <a href="{{ route('login') }}">Sign in</a></p>
                                     </div>
                                 </div>
                             </div>
